6dfa28166a4b6c799ab69d904a6319bb01f129da
Ryan Li <ryan.lichangrui@gmail.com>
2026-03-22 17:58:14 +0000

    update install operator script to always pull image

--- scripts/cluster/install_operator.sh
+++ scripts/cluster/install_operator.sh
@@ -8,6 +8,7 @@ source "${REPO_ROOT}/scripts/env.sh"
 echo "Installing Flink Kubernetes Operator on cluster..."
 helm install flink-kubernetes-operator \
     "${HELM_CHART}" \
+    --set "image.pullPolicy=Always" \
     --set "image.repository=${REGISTRY}/${OPERATOR_IMAGE_NAME}" \
     --set "image.tag=${OPERATOR_IMAGE_TAG}" \
     -f "${AUTOSCALER_VALUES}"
